070c114dd867687017f2e2264a20d3d308e65bff
Rudraksh Mehta <rudymehta2@gmail.com>
2026-06-08 21:32:02 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if s == s[::-1]:
+            return True
+
+        for i in range(len(s)):
+            t = s[:i] + s[i+1:]
+
+            if t == t[::-1]:
+                return True
+
+        return False
